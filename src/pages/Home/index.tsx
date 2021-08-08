7e25811ffdaa301b05ec218d2ab2e140b0490295
Rafael Domingues <rafaelcodomingues@gmail.com>
2021-08-08 15:07:13 -0300

    basic project structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Logo from '../../assets/WP.png';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <header>
+        <img src={Logo} alt="WP" />
+      </header>
+    </>
+  );
+};
+
+export default Home;
