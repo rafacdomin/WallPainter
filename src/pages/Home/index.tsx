2605159496d1f4661faaf0ce0167c6ea7ac12494
Rafael Domingues <rafaelcodomingues@gmail.com>
2021-08-09 13:12:40 -0300

    mobile page structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,25 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { About } from '../../components/About';
+import { Calculate } from '../../components/Calculate';
+import { Info } from '../../components/Info';
 
-import Logo from '../../assets/WP.png';
-import Background from '../../assets/background.jpg';
-
-import { Header, Main } from './styles';
+import { Main } from './styles';
 
 const Home: React.FC = () => {
+  const calcRef = useRef<HTMLDivElement>(null);
+
+  const scrollToCalc = useCallback(() => {
+    calcRef?.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [calcRef]);
+
   return (
     <>
-      <Header>
-        <div>
-          <img src={Logo} alt="WP" />
-        </div>
-      </Header>
       <Main>
-        <section>
-          <div>
-            <h1>Wall Paint Calculator</h1>
-            <p>
-              Find out the amount of paint needed to paint your room in a quick
-              and simple way.
-            </p>
-            <button type="button">GET START</button>
-          </div>
-
-          <img src={Background} alt="Cool Paint" />
-        </section>
+        <About action={scrollToCalc} />
+        <Info />
+        <div ref={calcRef}>
+          <Calculate />
+        </div>
       </Main>
     </>
   );
